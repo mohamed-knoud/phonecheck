53514d32f9a47271b5207a714d07c013031fe18a
mohamed-knoud <117742106+mohamed-knoud@users.noreply.github.com>
2024-09-28 21:40:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('check').addEventListener('click',function(){
-    fetch(`http://apilayer.net/api/validate?access_key=216ab70f0035ae73c01c9c4ece6dedf8&number=${document.getElementById('num').value}&country_code=MA&format=1`)
+    fetch(`https://apilayer.net/api/validate?access_key=216ab70f0035ae73c01c9c4ece6dedf8&number=${document.getElementById('num').value}&country_code=MA&format=1`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
